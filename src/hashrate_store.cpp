6723f745752d91773e26211f7a8d858034a5b51a
Pokio Project <pokiochain@gmail.com>
2025-06-02 23:44:45 +0200

    initial commit
    xmrig-6.22.2

---
+++ src/hashrate_store.cpp
@@ -0,0 +1,5 @@
+#include "hashrate_store.h"
+
+namespace xmrig {
+    double g_last_10m_hashrate = 0.0;  // Definición real de la variable
+} // namespace xmrig
---
+++ src/hashrate_store.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace xmrig {
+    extern double g_last_10m_hashrate;  // Declaración externa
+} // namespace xmrig
